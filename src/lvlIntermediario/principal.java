2729642aaa029ba2ed97255f15f30bac95844f90
Gabriel Menezes <gabrielslmenezes@gmail.com>
2026-01-27 21:27:17 -0300

    Começando um menu.

--- src/lvlIntermediario/principal.java
+++ src/lvlIntermediario/principal.java
@@ -1,9 +1,58 @@
 package lvlIntermediario;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class principal {
     static void main() {
 
-        System.out.println("Estes são os ninjas");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=== Cadastre seu Ninja ===");
+
+        //ARRAY 5
+        final int TAMANHO_MAX = 5;
+        String[] ninjas = new String[TAMANHO_MAX];
+
+        int registrados = 0;
+
+        int escolha = 0;
+
+
+        //LOOP MENU
+        while (escolha != 4){
+            //MENU
+            System.out.println("1: Registrar Ninja");
+            System.out.println("2: Exibir lista");
+            System.out.println("3: Atualizar habilidade");
+            System.out.println("4: Sair");
+
+            escolha = sc.nextInt();
+            switch (escolha){
+                case 1:
+                    if (registrados <= TAMANHO_MAX){
+                        System.out.println("Vamos cadastrar!");
+                        String nomeNinja = sc.next();
+                        ninjas[registrados]= nomeNinja;
+                    }else {
+                        System.out.println("O sistema está cheio");
+                    }
+
+                break;
+                case 2:
+
+                    break;
+
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+                default:
+            }
+        }
 
 
         uchiha shisui = new uchiha();
@@ -13,7 +62,6 @@ public class principal {
         shisui.statusMissao = "Concluida";
         shisui.mostrarInformacoes();
 
-        shisui.mostrarHabilidade();
 
 
 
