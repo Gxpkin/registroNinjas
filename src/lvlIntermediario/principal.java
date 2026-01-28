dd1043943f738c10403d62f09aca3e081345067a
Gabriel Menezes <gabrielslmenezes@gmail.com>
2026-01-28 16:36:16 -0300

    objeto/scanner

--- src/lvlIntermediario/principal.java
+++ src/lvlIntermediario/principal.java
@@ -1,9 +1,9 @@
 package lvlIntermediario;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class principal {
+
     static void main() {
 
         Scanner sc = new Scanner(System.in);
@@ -12,15 +12,14 @@ public class principal {
 
         //ARRAY 5
         final int TAMANHO_MAX = 5;
-        String[] ninjas = new String[TAMANHO_MAX];
+        String[] ninjasArray = new String[TAMANHO_MAX];
 
         int registrados = 0;
-
         int escolha = 0;
 
 
         //LOOP MENU
-        while (escolha != 4){
+        while (escolha != 4) {
             //MENU
             System.out.println("1: Registrar Ninja");
             System.out.println("2: Exibir lista");
@@ -28,43 +27,66 @@ public class principal {
             System.out.println("4: Sair");
 
             escolha = sc.nextInt();
-            switch (escolha){
+            //sc.nextLine();
+
+            switch (escolha) {
                 case 1:
-                    if (registrados <= TAMANHO_MAX){
+                    //PARA VOLTAR FUNCIONAR APENAS RETONAR O CODIGO ORIGINAL COMENTADO
+                    if (registrados <= TAMANHO_MAX) {
                         System.out.println("Vamos cadastrar!");
+
+
+                        // PRECISO DAR UM JEITO DE CRIAR UM OBJETO COM INFORMAÇOES DO USUARIO USANDO SCANNER
+                       /* ninja naruto = new ninja();
+                        System.out.println("Digite o nome do seu ninja");
+                        naruto.nome = sc.nextLine();
+                        System.out.println("Qual sua idade");
+                        naruto.idade = sc.nextInt();
+                        System.out.println("De qual aldeia você é");
+                        sc.nextLine();
+                        naruto.aldeia = sc.nextLine();
+                        System.out.println("Qual status da sua missao ?");
+                        naruto.statusMissao = sc.nextLine();
+                        naruto.mostrarInformacoes();
+                        */
+
+                        // CÓDIGO ORIGINAL, ADICIONANDO APENAS STRING NOME
                         String nomeNinja = sc.next();
-                        ninjas[registrados]= nomeNinja;
-                    }else {
+                        ninjasArray[registrados] = nomeNinja;
+                        registrados++;
+
+                    } else {
                         System.out.println("O sistema está cheio");
                     }
 
-                break;
+                    break;
                 case 2:
-
+                    for (String ninja : ninjasArray) {
+                        System.out.println(ninja);
+                    }
                     break;
+                case 3: {
 
-                case 3:
+                    }break;
+                case 4: {
+                    System.out.println("Estamos finalizando o sistema em 3...2...1.");
 
-                    break;
-
-                case 4:
-
-                    break;
-                default:
+                    }break;
+                default:{
+                    System.out.println("Opção Inválida, Tente Novamente");
+                }
             }
         }
 
 
-        uchiha shisui = new uchiha();
-        shisui.nome = "Shisui Uchiha";
-        shisui.idade = 16;
-        shisui.aldeia = "Folha";
-        shisui.statusMissao = "Concluida";
+    uchiha shisui = new uchiha();
+    shisui.nome ="Shisui Uchiha";
+    shisui.idade =16;
+    shisui.aldeia ="Folha";
+    shisui.statusMissao ="Concluida";
+
         shisui.mostrarInformacoes();
 
 
-
-
-
+        }
     }
-}
